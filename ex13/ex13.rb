e9e489d80d6a49a67b46223b93447b8eab0f779c
Tracy <tracyho0867@gmail.com>
2018-02-14 18:11:11 -0500

    Adding completed ex13

---
+++ ex13/ex13.rb
@@ -0,0 +1,12 @@
+first, second, third = ARGV # Argument variable
+
+first = $stdin.gets.chomp
+second = $stdin.gets.chomp
+third = $stdin.gets.chomp
+# ?????????????
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+# Run with below scripts:
+# $ ruby ex13.rb first 2nd 3rd
