1941f0c72b4c90c042a8d2e8b2036de8368b3f89
Tracy <tracyho0867@gmail.com>
2018-03-16 20:15:22 -0400

    Updating ex20 notes

--- ex20/ex20.rb
+++ ex20/ex20.rb
@@ -1,5 +1,5 @@
 input_file = ARGV.first
-
+#gather txt file name from user
 def print_all(f)
   puts f.read
 end
@@ -7,28 +7,29 @@ end
 def rewind(f)
   f.seek(0)
 end
-
+#
 def print_a_line(line_count, f)
   puts "#{line_count}, #{f.gets.chomp}"
 end
 
 current_file = open(input_file)
-
+# to open the user file
 puts "First let's print the whole file:\n"
 
 print_all(current_file)
-
+# print the file
 puts "Now let's rewind, kind of like a tape."
-
+# seek to position 0, the beginning of the file
 rewind(current_file)
-
+# need to rewind
 puts "Let's print three lines:"
 
 current_line = 1
 print_a_line(current_line, current_file)
-
+# print the first line of the current file
 current_line = current_line + 1
 print_a_line(current_line, current_file)
-
+# print the 2nd line
 current_line = current_line + 1
 print_a_line(current_line, current_file)
+# print the 3rd line
